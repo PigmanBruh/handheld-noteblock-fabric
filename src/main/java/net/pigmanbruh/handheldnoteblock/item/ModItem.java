e7db07a272e66695c7ef99c31b263539cee9c35e
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-03-17 22:05:22 +0700

    Create ModItem.java

---
+++ src/main/java/net/pigmanbruh/handheldnoteblock/item/ModItem.java
@@ -0,0 +1,20 @@
+package net.pigmanbruh.handheldnoteblock.item.ModItem;
+
+import net.pigmanbruh.handheldnoteblock.Main
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItem {
+    public static final Item HANDHELD_NOTEBLOCK = new Item(new FabricItemSettings());
+
+    public void registerItem() {
+        Registry.register(Registries.ITEM, new Identifier(Main.Mod_ID, "handheld_noteblock"), HANDHELD_NOTEBLOCK);
+    }
+}
