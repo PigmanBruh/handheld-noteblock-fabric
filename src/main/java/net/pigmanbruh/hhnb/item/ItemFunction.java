a61d9a28bb67c73306a2be3576c7249a113641dc
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-03-18 08:33:10 +0700

    Update ItemFunction.java

--- src/main/java/net/pigmanbruh/hhnb/item/ItemFunction.java
+++ src/main/java/net/pigmanbruh/hhnb/item/ItemFunction.java
@@ -20,7 +20,7 @@ public class ItemFunction extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient) {
-            user.playSound(block.note_block.harp, SoundCategory.RECORDS, 1.0F, PITCH);
+            user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_HARP, SoundCategory.RECORDS, 1.0F, PITCH);
             user.getItemCooldownManager().set(this, 1);
         }
         return super.use(world, user, hand);
