f6968fba8416ee4113f19c35eeb9e938ab54d773
PigmanBruh <87263998+PigmanYT3531@users.noreply.github.com>
2023-03-18 08:15:04 +0700

    Update ModItem.java

--- src/main/java/net/pigmanbruh/hhnb/item/ModItem.java
+++ src/main/java/net/pigmanbruh/hhnb/item/ModItem.java
@@ -1,6 +1,7 @@
 package net.pigmanbruh.hhnb.item;
 
 import net.pigmanbruh.hhnb.Main;
+import net.pigmanbruh.hhnb.ItemFunction;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -12,9 +13,17 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItem {
-    public static final Item HANDHELD_NOTEBLOCK = new Item(new FabricItemSettings());
+    public static final Item HANDHELD_NOTEBLOCK = new ItemFunction(new FabricItemSettings());
 
     public static void registerItem() {
         Registry.register(Registries.ITEM, new Identifier(Main.Mod_ID, "handheld_noteblock"), HANDHELD_NOTEBLOCK);
+    }
+
+    public static void addItemsToItemGroups() {
+        addToItemGroup(ItemGroups.REDSTONE, HANDHELD_NOTEBLOCK);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
     }
 }
