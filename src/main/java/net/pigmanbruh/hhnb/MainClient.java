a25c4bb5592630e939453598480f0c8ff34f4a89
PigmanBruh <pigmanbruh4@gmail.com>
2023-03-19 10:20:07 +0700

    messing with keybind

--- src/main/java/net/pigmanbruh/hhnb/MainClient.java
+++ src/main/java/net/pigmanbruh/hhnb/MainClient.java
@@ -1,23 +1,14 @@
 package net.pigmanbruh.hhnb;
 
+import net.pigmanbruh.hhnb.item.Keybind;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.option.KeyBinding;
 
 public class MainClient implements ClientModInitializer {
 
-    private static KeyBinding keyBinding;
-        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-           "key.hhnb.change.note",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_Z,
-            "category.hhnb"
-        ));
-
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (keyBinding.wasPressed()) {
-                client.player.sendMessage(Text.literal("Key Z was pressed!"), false);
-            }
-        });
+        Keybind.registerKeybinds();
     }
 }
---
+++ src/main/java/net/pigmanbruh/hhnb/item/Keybind.java
@@ -0,0 +1,33 @@
+package net.pigmanbruh.hhnb.item;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class Keybind {
+
+    public static KeyBinding changenote;
+
+    public static void registerKeybinds {
+        changenote = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.hhnb.changenote", // The translation key of the keybinding's name
+            InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+            GLFW.GLFW_KEY_Z, // The keycode of the key
+            "category.hhnb" // The translation key of the keybinding's category.
+        ));
+    }
+
+    public static void registerKeyInput {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                client.player.sendMessage(Text.literal("Key Z was pressed!"), false);
+            }
+        });
+
+    registerKeyInput();
+
+    }
+}
